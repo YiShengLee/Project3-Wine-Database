dbb85d127b321cc61f943e4b462eeeab9422be69
Ubuntu <ubuntu@ip-172-31-44-210.ec2.internal>
2019-12-23 13:22:18 +0000

    Added flask pymongo

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+import re
+
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
