85a80a1597197532ca7d792748d2a614473d91ca
Ubuntu <ubuntu@ip-172-31-44-210.ec2.internal>
2019-12-25 02:13:08 +0000

    Added CSS file

--- app.py
+++ app.py
@@ -2,14 +2,29 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import pymongo
 import re
 
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_DBNAME"] = 'Project_3'
+app.config["COLLECTION_NAME"] = 'wine'
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+conn = pymongo.MongoClient(os.getenv("MONGO_URI"))
 
+# Initilize Pymongo
+mongo = PyMongo(app)
 
+# Main Route
+@app.route('/')
+@app.route('/index')
+# def index():
+#     wine = conn[app.config["MONGO_DBNAME"]][app.config["COLLECTION_NAME"]].find()
+#     return render_template('index.html', wine = wine)
+
+def index():
+    return render_template("index.html", 
+                          index=mongo.db.wine.find())
 
 
 
