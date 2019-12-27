2fd5e8eac7e1593e112681bea0aec2b80e2d11a0
Ubuntu <ubuntu@ip-172-31-44-210.ec2.internal>
2019-12-27 13:15:01 +0000

    Add data into MongoDB

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 import re
@@ -36,11 +36,31 @@ def index():
 @app.route('/add_wine')
 def add_wine():
     return render_template("add.html", title="add_wine")
+
+@app.route('/add_wine', methods=['POST']) #To add the new wine information to mongodb database
+def insert_wine():
+    name = request.form.get('name')
+    points = request.form.get('points')
+    variety = request.form.get('variety')
+    title = request.form.get('title')
     
+    conn[DATABASE_NAME][COLLECTION_NAME3].insert({
+        "name" : name,
+        "points" : points,
+        "variety" : variety,
+        "title" : title
+    })
+    
+    print(name,points,variety,title)
+    # flash("You have added a new wine.")
+    return redirect("/")
+    
+# Add Search Section
 @app.route('/search')
 def search():
     wine = conn[DATABASE_NAME][COLLECTION_NAME].find()
     return render_template("search.html", title="search", wine=wine)
+
 
 
 
