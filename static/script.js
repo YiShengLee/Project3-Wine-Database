9dcde9efff49fe1729041d53d2356f0050aa7d79
Ubuntu <ubuntu@ip-172-31-44-210.ec2.internal>
2020-01-04 07:28:45 +0000

    Added form validaation

--- static/script.js
+++ static/script.js
@@ -6,23 +6,37 @@ $(document).ready(function(){
     firstname: {
             validators: {
                     stringLength: {
-                    min: 2,
-                },
+                        min: 2,
+                        max: 30,
+                        message: 'Firstname must be more than 2 and less than 30 characters long'
+                    },
                     notEmpty: {
-                    message: 'Please enter your First Name'
-                }
+                        message: 'Please enter your First Name'
+                    }
             }
-        },
-         lastname: {
+    },
+    lastname: {
             validators: {
-                 stringLength: {
-                    min: 2,
-                },
-                notEmpty: {
-                    message: 'Please enter your Last Name'
-                }
+                    stringLength: {
+                        min: 2,
+                        max: 30,
+                        message: 'Lastname must be more than 2 and less than 30 characters long'
+                    },
+                    notEmpty: {
+                        message: 'Please enter your Last Name'
+                    }
             }
-        },
+    },
+    price: {
+            validators: {
+                    integar: {
+                        message: 'The value is not an integar'
+                    },
+                    notEmpty: {
+                        message: 'Please enter the cost of the wine'
+                    }
+            }
+    },    
     
     }});
 });
